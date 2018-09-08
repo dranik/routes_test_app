5456ff4e89a1800a2d3c1bb8f56395effe3fdd54
estanislao <stlestrade@gmail.com>
2018-09-08 12:39:45 +0300

    Validation for the routepoints and route

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,4 +4,18 @@ class Route < ApplicationRecord
   validates_length_of :description, :maximum => 100, :allow_blank => true
   enum route_role: {driver:0, passenger:1}
   validates :seats, inclusion: 1..10
+  validate :validate_points
+
+  private
+
+  def validate_points
+    errors.add(:route_points, "start is missing") if count_points(:start_point) < 1
+    errors.add(:route_points, "finish is missing") if count_points(:finish_point) < 1
+    errors.add(:route_points, "too many starts") if count_points(:start_point) > 1
+    errors.add(:route_points, "too many finishes") if count_points(:finish_point) > 1
+  end
+
+  def count_points(point_type)
+    route_points.map(&:point_type).delete_if{|f| f != point_type.to_s}.count
+  end
 end
